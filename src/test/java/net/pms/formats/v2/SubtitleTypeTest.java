1eb431306b845c3dcf808597a370d84d404b437a
Samuel <sirimangala@googlemail.com>
2016-11-21 20:58:25 +0100

    Update SubtitleTypeTest.java

--- src/test/java/net/pms/formats/v2/SubtitleTypeTest.java
+++ src/test/java/net/pms/formats/v2/SubtitleTypeTest.java
@@ -77,7 +77,7 @@ public class SubtitleTypeTest {
 		assertThat(VOBSUB.getDescription()).isEqualTo("VobSub");
 		assertThat(USF.getDescription()).isEqualTo("Universal Subtitle Format");
 		assertThat(BMP.getDescription()).isEqualTo("Bitmap");
-		assertThat(DIVX.getDescription()).isEqualTo("DIVX subtitles");
+		assertThat(DIVX.getDescription()).isEqualTo("DivX subtitles");
 		assertThat(TX3G.getDescription()).isEqualTo("MPEG-4 Timed Text");
 		assertThat(PGS.getDescription()).isEqualTo("Presentation Grapic Stream");
 		assertThat(WEBVTT.getDescription()).isEqualTo("Web Video Text Tracks");
