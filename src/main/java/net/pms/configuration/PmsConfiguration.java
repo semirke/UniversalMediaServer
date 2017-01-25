a62eda65dad0fef8a0dcf933582b6a6ce449dcf9
Nadahar <Nadahar@users.noreply.github.com>
2017-01-25 15:19:07 +0100

    - Refactored/Implemented transcoding with DCRaw:   - Renamed RAWThumbnailer to DCRaw as its no longer only making thumbnails   - More or less completely refactored RAWThumbnailer/DCRaw   - Renamed DCRaw "configuration engine name" from "rawthumbs" to "dcraw"   - Created abstract class ImagePlayer   - Refactored Player.equals() and implemented Player.hashCode()   - Moved the DCRaw process handling from RAW.parse() to DCRaw.parse()   - Implemented image transcoding in Request and RequestV2

--- src/main/java/net/pms/configuration/PmsConfiguration.java
+++ src/main/java/net/pms/configuration/PmsConfiguration.java
@@ -2267,7 +2267,7 @@ public class PmsConfiguration extends RendererConfiguration {
 				"mencoderwebvideo",
 				"vlcaudio", // (VideoLanAudioStreaming) TODO (legacy web audio engine): remove
 				"ffmpegdvrmsremux",
-				"rawthumbs"
+				"dcraw"
 			},
 			","
 		);
@@ -2281,6 +2281,12 @@ public class PmsConfiguration extends RendererConfiguration {
 		);
 
 		engines = hackAvs(registry, engines);
+		// Backwards compatibility, can be removed when sufficient time has passed - 2017-01
+		int i = engines.indexOf("rawthumbs");
+		if (i >= 0) {
+			engines.set(i, "dcraw");
+		}
+
 		return engines;
 	}
 
