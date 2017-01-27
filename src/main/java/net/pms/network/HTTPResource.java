943a2a5255e08a581b24d2f47bbdef93f0ca000c
Samuel <sirimangala@googlemail.com>
2017-01-27 18:00:59 +0100

    Update HTTPResource.java

--- src/main/java/net/pms/network/HTTPResource.java
+++ src/main/java/net/pms/network/HTTPResource.java
@@ -65,7 +65,7 @@ public class HTTPResource {
 	public static final String AUDIO_MPA_TYPEMIME = "audio/mpeg";
 	public static final String AUDIO_MPC_TYPEMIME = "audio/x-musepack";
 	public static final String AUDIO_OGG_TYPEMIME = "audio/ogg";
-	public static final String AUDIO_QCELP_TYPEMIME = "audio/qcelp";
+	public static final String AUDIO_QLCM_TYPEMIME = "audio/qcelp";
 	public static final String AUDIO_RA_TYPEMIME = "audio/x-realaudio";
 	public static final String AUDIO_SHN_TYPEMIME = "audio/x-shn";
 	public static final String AUDIO_THREEGPPA_TYPEMIME = "audio/3gpp";
